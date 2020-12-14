7425423397bbda801750c96f7aec398719cd247d
Bach Mai <bachmai@sunshinegroup.vn>
2020-12-14 17:50:49 +0700

    Add implementation for Array and Doubly linked list

---
+++ array.py
@@ -0,0 +1,55 @@
+class Array:
+    def __init__(self, size):
+        self.capacity = 1
+        self.items = [None]*capacity
+        self.count = self.count(items)
+
+    def size(self):
+        return count
+
+    def capacity(self):
+        return capacity
+
+    def isEmpty(self):
+        return count == 0
+
+    def itemAt(self, index):
+        return items[index]
+
+    def append(self, item):
+        if (count == 0):
+            items[0] = item
+        else:
+            items[count] = item
+        self.count += 1
+        
+    def insert(self, item, index):
+        if (index < 0 or index > count):
+            return
+        else:
+            if (count == capacity):
+                capacity *= 2
+            items.insert(index, item)
+            count += 1
+    
+    def pop(self):
+        if (count == 0):
+            return None
+        else:
+            count -= 1
+            return pop(items)
+    
+    def removeAt(self, index):
+        if (index < 0 or index >= self.count):
+            return
+        else:
+            if (self.count == capacity * 0.2 + 1):
+                capacity /= 2
+            items.remove(index)
+            count -= 1
+
+    def count(self, lst):
+        count = 0
+        while count < len(lst):
+            count += 1
+        return count
---
+++ doubly_linked_list.py
@@ -0,0 +1,142 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.before = None
+        self.after = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def size(self):
+        return length
+
+    def isEmpty(self):
+        return length == 0
+
+    def valueAt(self, index):
+        if (index < 0 or index >= length):
+            return None
+        current = head
+        count = 0
+        while (current):
+            if (count == index):
+                return current.value
+            count += 1
+            current = current.after
+        return None
+
+    def pushFront(self, value):
+        node = Node(value)
+        if (length == 0):
+            self.head = node
+            self.tail = node
+        else:
+            self.head.before = node
+            node.after = self.head
+            self.head = node
+        length += 1
+
+    def popFront(self):
+        if (length == 0):
+            return None
+        else:
+            oldHead = self.head
+            if (length == 1):
+                self.head = None
+                self.head = None
+                return oldHead.value
+            else:
+                nextHead = oldHead.after
+                nextHead.before = None
+                self.head = nextHead
+                return oldHead.value
+        length -= 1
+
+    def pushBack(self, value):
+        node = Node(value)
+        if (length == 0):
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.after = node
+            node.before = self.tail
+            self.tail = node
+        length += 1
+
+    def popBack(self):
+        if (length == 0):
+            return None
+        else:
+            oldTail = self.tail
+            if (length == 1):
+                self.head = None
+                self.tail = None
+                return oldTail.value
+            else:
+                nextTail = oldTail.before
+                nextTail.after = None
+                self.tail = nextTail
+                return oldTail.value
+        length -= 1
+
+    def front(self):
+        if (length == 0):
+            return None
+        return self.head.value
+
+    def back(self):
+        if (length == 0):
+            return None
+        return self.tail.value
+
+    def insert(self, value, index):
+        if (index < 0 or index > self.length):
+            return
+        elif (index == 0):
+            self.pushFront(value)
+        elif (index == length):
+            self.pushBack(value)
+        else:
+            node = Node(value)
+            count = 0
+            current = head
+            before = None
+            after = None
+            while (current):
+                if (count == index):
+                    before = current
+                    after = current.next
+                    break
+                count += 1
+            node.before = before
+            before.next = node
+            node.after = after
+            after.before = node
+        length += 1
+
+    def removeAt(self, index):
+        if (index < 0 or index > self.length):
+            return
+        elif (index == 0):
+            self.popFront()
+        elif (index == length):
+            self.popBack()
+        else:
+            count = 0
+            current = head
+            before = None
+            after = None
+            while (current):
+                if (count == index):
+                    before = current
+                    after = current.after
+                    before.after = after
+                    after.before = before
+                    break;
+                count += 1
+        length -= 1
+        
