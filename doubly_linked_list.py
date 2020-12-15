433aed588e1a8895b43d58da9838d4ad5ecf8a43
Bach Mai <bachmai@sunshinegroup.vn>
2020-12-15 15:31:05 +0700

    Refactor doubly linked list

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -4,6 +4,8 @@ class Node:
         self.before = None
         self.after = None
 
+    def __str__(self):
+        return str(self.value)
 
 class DoublyLinkedList:
     def __init__(self):
@@ -12,15 +14,15 @@ class DoublyLinkedList:
         self.length = 0
 
     def size(self):
-        return length
+        return self.length
 
     def isEmpty(self):
-        return length == 0
+        return self.length == 0
 
     def valueAt(self, index):
-        if (index < 0 or index >= length):
+        if (index < 0 or index >= self.length):
             return None
-        current = head
+        current = self.head
         count = 0
         while (current):
             if (count == index):
@@ -31,21 +33,21 @@ class DoublyLinkedList:
 
     def pushFront(self, value):
         node = Node(value)
-        if (length == 0):
+        if (self.length == 0):
             self.head = node
             self.tail = node
         else:
             self.head.before = node
             node.after = self.head
             self.head = node
-        length += 1
+        self.length += 1
 
     def popFront(self):
-        if (length == 0):
+        if (self.length == 0):
             return None
         else:
             oldHead = self.head
-            if (length == 1):
+            if (self.length == 1):
                 self.head = None
                 self.head = None
                 return oldHead.value
@@ -54,25 +56,25 @@ class DoublyLinkedList:
                 nextHead.before = None
                 self.head = nextHead
                 return oldHead.value
-        length -= 1
+        self.length -= 1
 
     def pushBack(self, value):
         node = Node(value)
-        if (length == 0):
+        if (self.length == 0):
             self.head = node
             self.tail = node
         else:
             self.tail.after = node
             node.before = self.tail
             self.tail = node
-        length += 1
+        self.length += 1
 
     def popBack(self):
-        if (length == 0):
+        if (self.length == 0):
             return None
         else:
             oldTail = self.tail
-            if (length == 1):
+            if (self.length == 1):
                 self.head = None
                 self.tail = None
                 return oldTail.value
@@ -81,15 +83,15 @@ class DoublyLinkedList:
                 nextTail.after = None
                 self.tail = nextTail
                 return oldTail.value
-        length -= 1
+        self.length -= 1
 
     def front(self):
-        if (length == 0):
+        if (self.length == 0):
             return None
         return self.head.value
 
     def back(self):
-        if (length == 0):
+        if (self.length == 0):
             return None
         return self.tail.value
 
@@ -98,45 +100,56 @@ class DoublyLinkedList:
             return
         elif (index == 0):
             self.pushFront(value)
-        elif (index == length):
+        elif (index == self.length):
             self.pushBack(value)
         else:
             node = Node(value)
             count = 0
-            current = head
+            current = self.head
             before = None
             after = None
             while (current):
                 if (count == index):
-                    before = current
-                    after = current.after
+                    before = current.before
+                    after = current
                     break
+                current = current.after
                 count += 1
             node.before = before
             before.after = node
             node.after = after
             after.before = node
-        length += 1
+        self.length += 1
 
     def removeAt(self, index):
         if (index < 0 or index > self.length):
             return
         elif (index == 0):
             self.popFront()
-        elif (index == length):
+        elif (index == self.length - 1):
             self.popBack()
         else:
             count = 0
-            current = head
+            current = self.head
             before = None
             after = None
             while (current):
                 if (count == index):
-                    before = current
+                    before = current.before
                     after = current.after
                     before.after = after
                     after.before = before
                     break;
+                current = current.after
                 count += 1
-        length -= 1
+        self.length -= 1
         
+if __name__ == '__main__':
+    lst = DoublyLinkedList()
+    lst.pushFront(Node(2))
+    lst.pushFront(Node(0))
+    lst.pushBack(Node(5))
+    lst.insert(Node(1), 1)
+    lst.insert(Node(3), 3)
+    lst.insert(Node(4), 4)
+    lst.removeAt(3)
