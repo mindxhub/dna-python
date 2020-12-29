112a3fc7b17c6357c882f76d2225bc846b7935bf
Bach Mai <bachmai@sunshinegroup.vn>
2020-12-29 17:00:52 +0700

    Add queue and stack

---
+++ queue.py
@@ -0,0 +1,74 @@
+class QueueNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return str(self.value)
+
+class QueueLinkedList:
+    def __init__(self):
+        self.head;
+        self.tail;
+    
+    def add(self, item):
+        newNode = QueueNode(item)
+        if (self.tail != None):
+            self.tail.next = newNode
+        self.tail = newNode
+        if (self.head == None):
+            self.head = self.tail
+    
+    def pop(self):
+        if (self.head == None):
+            return
+        oldHead = self.head
+        self.head = self.head.next
+        if (self.head == None):
+            self.tail = None
+        return oldHead
+
+    def front(self):
+        if (self.head == None):
+            return
+        return self.head.value
+    
+    def isEmpty(self):
+        return self.head == None
+    
+class QueueArray:
+    def __init__(self):
+        self.items = []
+        self.allocated = 0
+        self.count = self.count(self.items)
+    
+    def add(self, item):
+        self.items += [item]
+        self.updateAlloc()
+        self.count += 1
+    
+    def pop(self):
+        if (self.count == 0):
+            return None
+        else:
+            firstItem = self.items[0]
+            self.count -= 1
+            self.updateAlloc()
+            self.items = self.items[1:]
+            return firstItem
+
+    def front(self):
+        return self.queue[0]
+    
+    def isEmpty(self):
+        return self.count == 0
+    
+    def updateAlloc(self):
+        if (self.allocated == 0):
+            self.allocated = 1
+        elif (self.count == self.allocated):
+            self.allocated *= 2
+        elif (self.count < self.allocated*0.2):
+            self.allocated /= 2
+        else:
+            return
