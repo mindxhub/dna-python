112a8a2913ad5441f42725c2d7f336864ab29ae8
Bach Mai <bachmai@sunshinegroup.vn>
2021-01-05 15:07:25 +0700

    Formatting

--- hashmap.py
+++ hashmap.py
@@ -1,13 +1,14 @@
-from doubly_linked_list import DoublyLinkedList;
+from doubly_linked_list import DoublyLinkedList
+
 
 class Hashmap:
     def __init__(self):
         hashmap = {}
         buckets = 0
-    
+
     def hashFunction(self, value):
-        return value % 5;
-    
+        return value % 5
+
     def checkExist(self, value, hashValue):
         if self.buckets == 0:
             return False
@@ -18,19 +19,18 @@ class Hashmap:
                 return True
             cur = cur.next
         return False
-    
-    
+
     def insertToHashmap(self, value):
-        hashValue = self.hashFunction(value);
+        hashValue = self.hashFunction(value)
         if self.hashmap.has_key(hashValue):
             self.hashmap[hashValue].pushBack(value)
         else:
-            newLinkList = DoublyLinkedList();
+            newLinkList = DoublyLinkedList()
             newLinkList.pushBack(value)
             self.hashmap[hashValue] = newLinkList
-    
+
     def removeFromHashmap(self, value):
-        hashValue = self.hashFunction(value);
+        hashValue = self.hashFunction(value)
         if self.hashmap.has_key(hashValue):
             self.hashmap[hashValue].remove(value)
             if self.hashmap[hashValue].length == 0:
